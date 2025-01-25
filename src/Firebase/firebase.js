79c650399b019b078e67ee44f9643e085f1c40bf
shishirdubey24 <shishirdubey221@gmail.com>
2025-01-25 13:36:02 +0530

    added improvement

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAouUquFi8RZs0dOMQq9l9bLd03opKDc1A",
-  authDomain: "task-project-e0c35.firebaseapp.com",
-  projectId: "task-project-e0c35",
-  storageBucket: "task-project-e0c35.firebasestorage.app",
-  messagingSenderId: "1003508106735",
-  appId: "1:1003508106735:web:213e8f28706940d27e621a",
-  measurementId: "G-Z8NSLTP4EF",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
